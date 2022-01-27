c4616f06b5e1824b0604ef49c6e91a4352f0eab4
CL Bogart <clbogart3@outlook.com>
2022-01-26 18:38:56 -0700

    Add project files.

---
+++ Say_My_Name_Again_CB/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Say_My_Name_Again_CB")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP Inc.")>
+<Assembly: AssemblyProduct("Say_My_Name_Again_CB")>
+<Assembly: AssemblyCopyright("Copyright © HP Inc. 2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("965699a0-45db-4424-8462-d7f7382bea4b")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ Say_My_Name_Again_CB/SayMyNameAgain.vb
@@ -0,0 +1,38 @@
+﻿'Carson Bogart
+'RCET 0265
+'Spring 2022
+'Say My name...Again
+'
+
+
+Option Explicit On
+Option Strict On
+Option Compare Text
+Module SayMyNameAgain
+
+    Sub Main()
+        Dim userResponse As String
+        'asks for user input
+        Console.WriteLine("Enter your name")
+        userResponse = Console.ReadLine()
+        'userRespnse will select between the below code
+        If userResponse = "Emily" Then
+            'if emily is typed then the message will display
+            Console.WriteLine("Hello Emily!")
+        ElseIf userResponse = "Joe" Then
+            'if joe is typed then the message will display
+            Console.WriteLine("Howdy Joe!")
+        ElseIf userResponse = "Carson" Then
+            Console.WriteLine("Hey Carson I was hoping you'd type your name")
+            Console.WriteLine("You have a pretty cool name")
+            Console.Beep()
+        Else
+            'will only trigger if none of the above is typed
+            Console.WriteLine("Uh oh I'm broke now")
+        End If
+        'promtps the user how to exit the program
+        Console.WriteLine("Press enter to exit")
+        Console.ReadLine()
+    End Sub
+
+End Module
